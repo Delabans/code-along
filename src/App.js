8d5b4ea11612e0ad2a4f86789cd82beddf375a3b
Mcbraingh <benjamintackie@ymail.com>
2022-03-29 15:17:08 -0700

    React component 6

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
-import writers from "./writers";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
-  // console.log(writers);
-  return (
-    <div>
-      <h1>WRITE PROFILES</h1>
+  const [posts, setPosts] = useState([]);
 
-      <div className="container">
-        {writers.map((writer) => (
-          <div className="card">
-            <img
-              src={`/images/${writer.avatar}.png`}
-              width="300px"
-              height="300px"
-              alt=""
-            />
-            <div className="textGroup">
-              <p>audre audre_lorde</p>
-              <p>audre@email.com</p>
-              <p>+233551234438</p>
+  useEffect(() => {
+    (async () => {
+      let response = await Axios({
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/posts",
+      });
+
+      setPosts(response.data);
+    })();
+  });
+
+  return (
+    <div className="app">
+      <h1> Daily Posts </h1>
+      <div>
+        <div className="list">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
